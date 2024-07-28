a47048e37d5c7297e4afc1fc0b0cec79a447ff3b
hans-aniket <hansaniket06@gmail.com>
2024-07-29 05:11:11 +0530

    function for humanchoice done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getComputerChoice( _string1, _string2, _string3)
 {
     let randomValue = Math.random()
-    if (randomValue <= 0.5)
+    console.log("computer's choice")
+    if (randomValue <= 0.4)
     {
         console.log(_string1)
     }
@@ -18,3 +19,27 @@ let _string1 = "rock";
 let _string2 = "paper";
 let _string3 = "scissors";
 getComputerChoice(_string1,_string2,_string3);
+
+function getHumanChoice(_string1, _string2, _string3)
+{
+    let humanChoice = prompt("choose:rock,paper or scissors");
+    if (humanChoice == "rock")
+    {
+        alert("you chose rock")
+    }
+    else if (humanChoice == "paper")
+    {
+        alert("you chose paper")
+    }
+    else if (humanChoice == "scissors")
+    {
+        alert("you chose scissors")
+    }
+    else {
+        alert("invalid input")
+    }
+}
+getHumanChoice()
+
+let humanScore = 0;
+let computerScore = 0;
